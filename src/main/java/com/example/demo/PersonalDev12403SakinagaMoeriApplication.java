346a7d0fac037bf5b9e37de507606c074ee8a958
student <student@FJ-1025>
2026-05-19 10:27:02 +0900

    新規登録

---
+++ src/main/java/com/example/demo/PersonalDev12403SakinagaMoeriApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev12403SakinagaMoeriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev12403SakinagaMoeriApplication.class, args);
+	}
+
+}
